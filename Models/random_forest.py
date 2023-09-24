225559e09d6ecb6ab27356c1f8ed88c4753b9601
Anas Elbattra <75434006+AnasElbattra@users.noreply.github.com>
2023-09-25 02:28:16 +0300

    ds

--- Models/random_forest.py
+++ Models/random_forest.py
@@ -1,9 +1,9 @@
-from sklearn.ensemble import RandomForestClassifier
-
-
-class RandomForestModel:
-
-    def __init__(self):
-        return
-
-    def anas(self):
+# from sklearn.ensemble import RandomForestClassifier
+#
+#
+# class RandomForestModel:
+#
+#     def __init__(self):
+#         return
+#
+#     def anas(self):
